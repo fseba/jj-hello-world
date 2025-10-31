234f94f8f277dfdcfd6877d6391a991fa4cdcb9d
Sebastian Frueh <sfrueh@gmx.net>
2025-10-31 15:11:31 +0100

    add more comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // This is a simple Rust program that prints "Hello, world!" to the console.
+// another line added for demonstration purposes.
 fn main() {
     println!("Hello, world!");
     println!("Goodbye, world!");
