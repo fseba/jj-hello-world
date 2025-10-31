51f3c91cb9ad03baff74e490bf2ebe2f8b12165f
Sebastian Frueh <sfrueh@gmx.net>
2025-10-31 15:23:46 +0100

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 // This is a simple Rust program that prints "Hello, world!" to the console.
 // another line added for demonstration purposes.
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
